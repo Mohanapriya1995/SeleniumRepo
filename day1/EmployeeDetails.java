734c25782e7e1e4f1e8fd4503077fed2872003fe
Mohanapriya1995 <161923100+Mohanapriya1995@users.noreply.github.com>
2025-08-17 11:29:46 +0530

    Add files via upload
    First week day1 task completed

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,31 @@
+package firstweek.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		
+		String name = "John Doe";
+		   byte age = 30;
+			float salary = 55000.75f;
+			char gender = 'M';
+			boolean acceptTerms = true;
+			float rating = 4.5f;
+			long mobileNumber = 9876543210L;
+			int pinCode = 56001;
+				byte luckyNumber = 7;
+				
+				System.out.println("name="+name);
+				System.out.println("age="+age);
+				System.out.println("salary="+salary);
+				System.out.println("acceptTerms="+acceptTerms);
+				System.out.println("mobileNumber="+mobileNumber);
+				System.out.println("pinCode="+pinCode);
+				System.out.println("luckyNumber="+luckyNumber);
+				System.out.println("rating="+rating);
+				System.out.println("gender="+gender);	
+				
+				
+
+	}
+
+}
